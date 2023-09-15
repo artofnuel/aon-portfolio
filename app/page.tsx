29af5a9fbe5e4a896a5cd584c9565eb14b7113a2
Emmanuel Inua <artofnuel@gmail.com>
2023-09-15 13:43:41 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Hero from "./components/Hero";
 
 export default async function Home() {
   return (
-    <section className="w-full mx-auto p-4 md:p-8 md:px-12  overflow-hidden">
+    <section className="w-full mx-auto p-4 md:p-8 md:px-12 overflow-hidden">
       <Hero />
     </section>
   );
